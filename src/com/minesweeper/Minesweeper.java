1de182b8bffe0d21ed3da5abac9a770421485316
tiostwn <53292102+tiostwn@users.noreply.github.com>
2023-05-18 15:59:59 +0700

    add 3 function in Minesweeper.java

--- src/com/minesweeper/Minesweeper.java
+++ src/com/minesweeper/Minesweeper.java
@@ -55,7 +55,109 @@ public class Minesweeper extends JFrame {
   }
 
   private void countSurroundingMines() {
-   
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        if (!mines[i][j]) {
+          int count = 0;
+          if (i > 0 && mines[i - 1][j]) count++;
+          if (i < 9 && mines[i + 1][j]) count++;
+          if (j > 0 && mines[i][j - 1]) count++;
+          if (j < 9 && mines[i][j + 1]) count++;
+          if (i > 0 && j > 0 && mines[i - 1][j - 1]) count++;
+          if (i < 9 && j < 9 && mines[i + 1][j + 1]) count++;
+          if (i > 0 && j < 9 && mines[i - 1][j + 1]) count++;
+          if (i < 9 && j > 0 && mines[i + 1][j - 1]) count++;
+          surroundingMines[i][j] = count;
+        }
+      }
+    }
+  }
+
+  private void uncoverCell(int i, int j) {
+    if (mines[i][j]) {
+      loseGame();
+    } else {
+      buttons[i][j].setText(Integer.toString(surroundingMines[i][j]));
+      buttons[i][j].setEnabled(false);
+      uncoveredCells++;
+      if (uncoveredCells == 90) {
+        winGame();
+      }
+      if (surroundingMines[i][j] == 0) {
+        uncoverSurroundingCellsDFS(i, j);
+      }
+    }
+  }
+
+  private void uncoverSurroundingCellsDFS(int i, int j) {
+    Deque<Point> stack = new ArrayDeque<>();
+    stack.push(new Point(i, j));
+    visited[i][j] = true;
+
+    while (!stack.isEmpty()) {
+      Point point = stack.pop();
+      int x = point.x;
+      int y = point.y;
+
+      if (surroundingMines[x][y] == 0) {
+        if (x > 0 && !visited[x - 1][y]) {
+          uncoverCell(x - 1, y);
+          visited[x - 1][y] = true;
+          if (surroundingMines[x - 1][y] == 0) {
+            stack.push(new Point(x - 1, y));
+          }
+        }
+        if (x < 9 && !visited[x + 1][y]) {
+          uncoverCell(x + 1, y);
+          visited[x + 1][y] = true;
+          if (surroundingMines[x + 1][y] == 0) {
+            stack.push(new Point(x + 1, y));
+          }
+        }
+        if (y > 0 && !visited[x][y - 1]) {
+          uncoverCell(x, y - 1);
+          visited[x][y - 1] = true;
+          if (surroundingMines[x][y - 1] == 0) {
+            stack.push(new Point(x, y - 1));
+          }
+        }
+        if (y < 9 && !visited[x][y + 1]) {
+          uncoverCell(x, y + 1);
+          visited[x][y + 1] = true;
+          if (surroundingMines[x][y + 1] == 0) {
+            stack.push(new Point(x, y + 1));
+          }
+        }
+        if (x > 0 && y > 0 && !visited[x - 1][y - 1]) {
+          uncoverCell(x - 1, y - 1);
+          visited[x - 1][y - 1] = true;
+          if (surroundingMines[x - 1][y - 1] == 0) {
+            stack.push(new Point(x - 1, y - 1));
+          }
+        }
+        if (x < 9 && y < 9 && !visited[x + 1][y + 1]) {
+          uncoverCell(x + 1, y + 1);
+          visited[x + 1][y + 1] = true;
+          if (surroundingMines[x + 1][y + 1] == 0) {
+            stack.push(new Point(x + 1, y + 1));
+          }
+        }
+        if (x > 0 && y < 9 && !visited[x - 1][y + 1]) {
+          uncoverCell(x - 1, y + 1);
+          visited[x - 1][y + 1] = true;
+          if (surroundingMines[x - 1][y + 1] == 0) {
+            stack.push(new Point(x - 1, y + 1));
+          }
+        }
+        if (x < 9 && y > 0 && !visited[x + 1][y - 1]) {
+          uncoverCell(x + 1, y - 1);
+          visited[x + 1][y - 1] = true;
+          if (surroundingMines[x + 1][y - 1] == 0) {
+            stack.push(new Point(x +  1, y - 1));
+          }
+        }
+      }
+    }
   }
 
  
